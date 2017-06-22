803103e33855238742d1d33f92de6280770f28ad
rezeptpflichtig <lorenz.pressler@gmail.com>
2017-06-22 18:36:12 +0200

    removed not needed module

--- minim-listen.py
+++ minim-listen.py
@@ -1,7 +1,7 @@
 #!/usr/local/bin/env python3
 
 from pygtail import Pygtail
-import mutagen, time, re, subprocess, requests, json
+import mutagen, time, re, requests, json
 
 #path to you library
 path = '/media/archiv/'
